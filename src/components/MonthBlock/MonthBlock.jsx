bf288f3e48581bca6af2bef3b5eec2e6d236957e
FernandoBritoA <Fernando_BritoA@hotmail.com>
2020-07-02 18:21:02 -0500

    added mediaqueries to homepage for mobile view, added resultDisplay component

--- src/components/MonthBlock/MonthBlock.jsx
+++ src/components/MonthBlock/MonthBlock.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import './MonthBlock.css';
+import ResultDisplay from '../ResultDisplay/ResultDisplay';
 
 const MonthBlock = ({ month }) => {
-  console.log(month);
-  return <div></div>;
+  const { monthIndex } = month[0];
+
+  const getMonthInSpanish = () => {
+    const month = [];
+    month[0] = 'Enero';
+    month[1] = 'Febrero';
+    month[2] = 'Marzo';
+    month[3] = 'Abril';
+    month[4] = 'Mayo';
+    month[5] = 'Junio';
+    month[6] = 'Julio';
+    month[7] = 'Agosto';
+    month[8] = 'Septiembre';
+    month[9] = 'Octubre';
+    month[10] = 'Noviembre';
+    month[11] = 'Diciembre';
+
+    return month[monthIndex];
+  };
+  return (
+    <div className='month-block'>
+      <div className='month-header'>{getMonthInSpanish()}</div>
+      {month.map((results, i) => (
+        <ResultDisplay results={results} key={i} />
+      ))}
+    </div>
+  );
 };
 
 export default MonthBlock;
---
+++ src/components/ResultDisplay/ResultDisplay.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import './ResultDisplay.css';
+import grassBg from '../../assets/grass-texture.jpg';
+import local_image from '../../assets/venados-logo.png';
+
+const ResultDisplay = ({ results }) => {
+  const { away_score, home_score, opponent, opponent_image, local } = results;
+  const venados = 'Venados F.C.';
+
+  const [homeTeam, setHomeTeam] = useState({ name: '', image: '' });
+  const [awayTeam, setAwayTeam] = useState({ name: '', image: '' });
+  useEffect(() => {
+    if (local) {
+      setHomeTeam({ name: venados, image: local_image });
+      setAwayTeam({ name: opponent, image: opponent_image });
+    } else {
+      setHomeTeam({ name: opponent, image: opponent_image });
+      setAwayTeam({ name: venados, image: local_image });
+    }
+  }, [local, opponent, opponent_image]);
+
+  return (
+    <div className='result-display'>
+      <div className='bg-image-container'>
+        <div
+          className='bg-image'
+          style={{
+            backgroundImage: `url(${grassBg})`,
+          }}
+        />
+      </div>
+      <div className='calendar-result'>
+        <div className='calendar'>MAYO</div>
+        <div className='result-container'>
+          <div className='team-info'>
+            <img src={homeTeam.image} alt='' className='team-logo' />
+            <p>{homeTeam.name}</p>
+          </div>
+          <span className='score'>{`${home_score} - ${away_score}`}</span>
+          <div className='team-info'>
+            <img src={awayTeam.image} alt='' className='team-logo' />
+            <p>{awayTeam.name}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResultDisplay;
