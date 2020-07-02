0838fdb0e5dbc61e9d249890a77857f22ac5141d
FernandoBritoA <Fernando_BritoA@hotmail.com>
2020-07-01 19:04:38 -0500

    finisged styling menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import './Menu.css';
+import logo from '../../assets/venados-logo.png';
+import MenuItem from '../MenuItem/MenuItem';
 
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectIsMenuHidden } from '../../redux/menu/menu.selectors';
 
 const Menu = ({ isMenuHidden }) => {
-  return <div className={`menu ${isMenuHidden ? '' : 'show'}`}></div>;
+  return (
+    <div className={`menu ${isMenuHidden ? '' : 'show'}`}>
+      <div className='header'>
+        <img src={logo} alt='venados' className='menu-logo' />
+      </div>
+      <MenuItem />
+      <MenuItem />
+      <MenuItem />
+    </div>
+  );
 };
 
 const mapStateToProps = createStructuredSelector({
---
+++ src/components/MenuItem/MenuItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MenuItem.css';
+
+const MenuItem = () => {
+  return (
+    <div className='menu-item active'>
+      <h2>Menu Item</h2>
+    </div>
+  );
+};
+
+export default MenuItem;
